4c13167664dda08d53894c41a564990e077f7f40
luis felipe <luis.s.mendes20@gmail.com>
2021-09-01 22:02:10 -0300

    ADD: escopo inicial

---
+++ Rest-With-spring-boot-Udemy/src/test/java/br/com/erudio/RestWithspringbootUdemy/RestWithSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.RestWithspringbootUdemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
